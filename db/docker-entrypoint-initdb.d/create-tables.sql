5524d29579750af175a7c2d87e88c79d119dd5bc
Igor <snoozzzy7@gmail.com>
2022-07-25 23:45:17 +0200

    DB - added description table, portfolio table

--- db/docker-entrypoint-initdb.d/create-tables.sql
+++ db/docker-entrypoint-initdb.d/create-tables.sql
@@ -1,9 +1,33 @@
 CREATE TABLE users (
-    id SERIAL NOT NULL PRIMARY KEY,
+    userId BIGINT NOT NULL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     email VARCHAR(30) NOT NULL UNIQUE,
-    PASSWORD VARCHAR(60) NOT NULL
+    password VARCHAR(60) NOT NULL,
+    date TIMESTAMP,
+    isAdmin BOOLEAN
 );
 
-INSERT INTO users (name, email, password)
+CREATE TABLE description (
+    descriptionId BIGINT NOT NULL PRIMARY KEY REFERENCES users (userId),
+    image BYTEA,
+    about TEXT,
+    contact JSON
+);
+
+CREATE TABLE portfolio (
+    portfolioId BIGINT NOT NULL PRIMARY KEY REFERENCES users (userId),
+    companyName VARCHAR(100) NOT NULL,
+    startDate DATETIME NOT NULL,
+    endDate DATETIME,
+    skills JSON,
+    description TEXT
+);
+
+ALTER TABLE USERS
+    ADD CONSTRAINT fk_portfolio FOREIGN KEY (userId) REFERENCES description (descriptionId),
+        CONSTRAINT fk_description FOREIGN KEY (userId) REFERENCES portfolio (portfolioId);
+
+BEGIN TRANSACTION;
+    INSERT INTO users (name, email, password)
     VALUES ('Igor Nadiein', 'nadiein@dev.com', '1234');
+COMMIT;
